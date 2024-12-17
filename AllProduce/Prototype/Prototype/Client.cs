db53395b6c4995ec598f59255e6c244fb1c3c58a
Its0wl17 <147279200+Its0wl17@users.noreply.github.com>
2024-12-17 16:04:59 +0200

    Add files via upload
    Add pattern prototype

---
+++ AllProduce/Prototype/Prototype/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Client
+    {
+        static void Main()
+        {
+            CreateHouse house = new CreateHouse("Дом в стиле хайтек");
+            CreateHouse cloneHouse = (CreateHouse) house.Clone();
+            Console.WriteLine($"Основной дом: {house.Name}");
+
+            Console.WriteLine($"\nСкопированный дом дом: {cloneHouse.Name}");
+        }
+    }
+}
---
+++ AllProduce/Prototype/Prototype/CreateHouse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class CreateHouse : Prototype, IHouse
+    {
+        public string Name { get; }
+
+        public CreateHouse(string name)
+        {
+            Name = name;
+        }
+
+        public Prototype Clone()
+        {
+            return new CreateHouse(Name);
+        }
+    }
+}
